1e677551cc10b70b3aaec3bc8c28a05fcff4faff
GtPadsk <padskociusgytis@gmail.com>
2024-11-12 08:27:42 +0200

    Added .env

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 await import('dotenv/config');
+import dotenv from 'dotenv';
 
 
 import taskRouter from "./src/route/task.js";
@@ -11,11 +12,12 @@ app.use(cors());
 
 app.use(express.json());
 
-mongoose
-    .connect("mongodb+srv://padskociusgytis:M257twswCSaS2xN3@cluster0.v1huw.mongodb.net/")
-    .then(() => console.log('Connected!'))
-    .catch(() => console.log('Bad connection'));
-
+// mongoose
+//     .connect("process.env.MONGO_CONNECTION")
+//     .then(() => console.log('Connected!'))
+//     .catch(() => {
+//         console.log('Bad connection');
+//     })
 
 app.use(taskRouter);
 
@@ -25,11 +27,11 @@ app.use((req, res) => {
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI, {
+        await mongoose.connect(process.env.MONGO_CONNECTION, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
-        console.log('MongoDB connected');
+        console.log('MongoDB Connected!');
     } catch (err) {
         console.error(err.message);
         process.exit(1);
@@ -38,8 +40,7 @@ const connectDB = async () => {
 
 connectDB();
 
-const port = 3000;
 
-app.listen(port, () => {
-    console.log('Server is running on port 3000');
+app.listen(process.env.PORT, () => {
+    console.log('Server is running on port ' + process.env.PORT);
 });
